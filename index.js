1bca4b4acaec75ace055e0580abda672439ff53e
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-08-30 02:19:20 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somenumber) {
+  let distancefee;
+  
+  if (somenumber <= 400) {
+    distancefee = 'This one is on me!';
+  } else if (somenumber <= 2000) {
+    distancefee = 'That will be twenty bucks.';
+  } else if (somenumber <= 2500) {
+    distancefee = 'I will gladly take your thirty bucks.';
+  } else {
+    distancefee = 'No can do.';
+  }
+  
+  return distancefee;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   return city === 'NYC'? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
 }
+
